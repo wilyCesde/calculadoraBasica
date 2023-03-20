bcbf5d295bec212a37a4a7354ca530e62313337d
wilyCesde <wruizza@cesde.net>
2023-03-20 15:09:49 -0500

    # On branch master # Your branch is up to date with 'origin/master'.

--- App.jsx
+++ App.jsx
@@ -1,216 +1,150 @@
-import { useState } from "react";
+
+import { useState } from 'react';
+import React, { useEffect } from 'react';
 import {
-  StyleSheet,
-  Text,
   View,
+  Text,
   TextInput,
   Button,
-  TouchableOpacity,
-} from "react-native";
-import Banner from "./components/Banner";
+  StyleSheet,
+  AsyncStorage
+} from 'react-native';
+import Banner from './components/Banner';
 export default function App() {
-  //Definir las variables de estado del componente
-  const [valor1, setValor1] = useState("");
-  const [valor2, setValor2] = useState("");
+  const [nota1, setnota1] = useState('');
+  const [nota2, setnota2] = useState('');
+  const [nota3, setnota3] = useState('');
+  const [nombre, setnombre] = useState('');
+  const [asignatura, setasignatura] = useState('');
   const [resultado, setResultado] = useState(0);
-  const [esValido, setEsvalido] = useState(false);
-  const [mensaje, setMensaje] = useState("");
+  const [id, setId] = useState('');
 
-  // Definir los métodos del componente
-  let calcular = (operador) => {
-    if (valor1 != "" && valor2 != "") {
-      setEsvalido(true);
-      setMensaje("Calculo realizado con Éxito");
-      let resulta = 0;
- switch (operador) {
-        case "+":
-          resulta = parseFloat(valor1) + parseFloat(valor2);
-          break;
-        case "-":
-          resulta = parseFloat(valor1) - parseFloat(valor2);
-          break;
-        case "/":
-          resulta = parseFloat(valor1) / parseFloat(valor2);
-          break;
-        case "*":
-          resulta = parseFloat(valor1) * parseFloat(valor2);
-          break;
-      }
+  const calcular = () => {
+    if (
+      nota1 !== '' &&
+      nota2 !== '' &&
+      nota3 !== ''
+    ) {
+      const resulta = parseFloat(
+        nota1 * 0.3 +
+        nota2 * 0.35 +
+        nota3 * 0.35
+      );
       setResultado(resulta);
-    } else {
-      setEsvalido(false);
-      setMensaje("Calculo no realizado");
     }
   };
+  const limpiar = () => {
+    setnota1('');
+    setnota2('');
+    setnota3('');
+    setnombre('');
+    setasignatura('');
+    setResultado(0);
+  };
 
-  return (
-    <View style={[styles.container, { flex: 1 }]}>
-      <View
-        style={[
-          styles.container,
-          styles.views,
-          {
-            flex: 1,
-            backgroundColor: "powederblue",
-            width: "100%",
-            height: "100%",
-            borderColor: "black",
-          },
-        ]}
-      >
-        <Banner name="react2"></Banner>
-      </View>
-      <View
-        style={[
-          styles.container,
-          styles.views,
-          {
-            flex: 3,
-            backgroundColor: "#F7FAED",
-            width: "100%",
-            height: "100%",
-            borderColor: "black",
-          },
-        ]}
-      >
-        <Text>Calculadora Básica</Text>
-        <TextInput
-          placeholder="Ingese valor 1"
-          style={styles.inputs}
-          onChangeText={(valor1) => setValor1(valor1)}
-          value={valor1}
-        ></TextInput>
-        <TextInput
-          placeholder="Ingese valor 2"
-          style={styles.inputs}
-          onChangeText={(valor2) => setValor2(valor2)}
-          value={valor2}
-        ></TextInput>
-        <Text>Resultado</Text>
-        <Text
-          style={[styles.inputs, { color: "blue", fontWeight: "bold" }]}
-          onChangeText={(resultado) => setResultado(resultado)}
-        >
-          {resultado.toFixed(2)}
-        </Text>
-        <View style={{ flexDirection: "row", marginTop: 20 }}>
-          <TouchableOpacity
-            style={[
-              {
-                backgroundColor: "blue",
-              },
-              styles.buttons,
-            ]}
-            onPress={() => calcular("+")}
-          >
-            <Text style={styles.textsTouchables}>+</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={[
-              {
-                backgroundColor: "blue",
-              },
-              styles.buttons,
-            ]}
-            onPress={() => calcular("-")}
-          >
-            <Text style={styles.textsTouchables}>-</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={[
-              {
-                backgroundColor: "blue",
-              },
-              styles.buttons,
-            ]}
-            onPress={() => calcular("/")}
-          >
-            <Text style={styles.textsTouchables}>/</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={[
-              {
-                backgroundColor: "blue",
-              },
-              styles.buttons,
-            ]}
-            onPress={() => calcular("*")}
-          >
-            <Text style={styles.textsTouchables}>*</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={{ marginTop: 10 }}>
-          <TouchableOpacity
-            style={[
-              {
-                backgroundColor: "blue",
-              },
-              styles.buttons,
-            ]}
-            onPress={() => {
-              setValor1("");
-              setValor2("");
-              setResultado(0);
-              setMensaje("");
-            }}
-          >
-            <Text style={styles.textsTouchables}>C</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-      <View>
-        <Text style={{ color: esValido ? "green" : "red" }}>{mensaje}</Text>
-      </View>
-      <View
-        style={[
-          styles.container,
-          styles.views,
-          {
-            backgroundColor: "#F2F9D4",
-            width: "100%",
-            height: "100%",
-            borderColor: "black",
-          },
-        ]}
-      >
-        <Text>Derechos reservados</Text>
-      </View>
+
+
+const buscar = () => {
+  if (typeof window !== "undefined" && window.localStorage) {
+    const nombreRecuperado = localStorage.getItem(`${id}-nombre`);
+    const asignaturaRecuperada = localStorage.getItem(`${id}-asignatura`);
+    const nota1Recuperada = localStorage.getItem(`${id}-nota1`);
+    const nota2Recuperada = localStorage.getItem(`${id}-nota2`);
+    const nota3Recuperada = localStorage.getItem(`${id}-nota3`);
+
+    if (
+      nombreRecuperado !== null &&
+      asignaturaRecuperada !== null &&
+      nota1Recuperada !== null &&
+      nota2Recuperada !== null &&
+      nota3Recuperada !== null
+    ) {
+      setnombre(nombreRecuperado);
+      setasignatura(asignaturaRecuperada);
+      setnota1(nota1Recuperada);
+      setnota2(nota2Recuperada);
+      setnota3(nota3Recuperada);
+      calcular();
+    } else {
+      console.log(`No se encontraron datos completos para el ID ${id}`);
+    }
+  } else {
+    console.log("localStorage no está disponible");
+  }
+};
+
+
+return (
+  <View style={styles.container}>
+
+    <View style={styles.bannerContainer}>
+
     </View>
-  );
-}
-
+    <View style={styles.inputsContainer}>
+      <View style={styles.row}>
+        <Text style={styles.label}>Identificación:</Text>
+        <TextInput placeholder="Identificación" onChangeText={setId} value={id} style={styles.input} />
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.label}>Nombre:</Text>
+        <TextInput placeholder="Nombre" onChangeText={setnombre} value={nombre} style={styles.input} />
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.label}>Asignatura:</Text>
+        <TextInput placeholder="Asignatura" onChangeText={setasignatura} value={asignatura} style={styles.input} />
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.label}>Nota 1 (30%):</Text>
+        <TextInput placeholder="Nota 1 (30%)" onChangeText={setnota1} value={nota1} style={styles.input} />
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.label}>Nota 2 (35%):</Text>
+        <TextInput placeholder="Nota 2 (35%)" onChangeText={setnota2} value={nota2} style={styles.input} />
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.label}>Nota 3 (35%):</Text>
+        <TextInput placeholder="Nota 3 (35%)" onChangeText={setnota3} value={nota3} style={styles.input} />
+      </View>
+      <Button title="Calcular" onPress={calcular} />
+      <Button title="Limpiar" onPress={limpiar} />
+      <Button title="Buscar" onPress={buscar} />
+      <Text style={styles.resultado}>Resultado: {resultado}</Text>
+    </View>
+  </View>
+);
+};
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  bannerContainer: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: 'powderblue'
   },
-  views: {
-    width: "100%",
-    height: "100%",
-    borderColor: "black",
-    borderWidth: 2,
-    borderRadius: 5,
+  inputsContainer: {
+    flex: 3,
+    backgroundColor: '#F7FAED',
+    padding: 20
   },
-  inputs: {
-    borderRadius: 10,
-    padding: 10,
-    width: 150,
-    borderWidth: 2,
-    borderColor: "blue",
-    textAlign: "center",
-    marginTop: 10,
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10
   },
-  buttons: {
-    borderRadius: 10,
-    padding: 10,
-    width: 60,
-    opacity: 0.5,
-    marginLeft: 10,
+  label: {
+    fontWeight: 'bold',
+    marginRight: 10
   },
-  textsTouchables: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
+  resultado: {
+    color: 'blue',
+    fontWeight: 'bold',
+    marginTop: 20
   },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10
+  }
 });
